0aba3f3cd34c3391b3c0c9ce00e1ba04781a5476
kyle-hassett <kylehassett72@gmail.com>
2019-02-14 23:52:21 -0600

    Update algo_practice.rb

--- algo_practice.rb
+++ algo_practice.rb
@@ -7,7 +7,7 @@ def invert(list)
 end
 
 def high_and_low(numbers)
-	numbers.split.map(&:to_i).minmax.reverse.join(" ")
+	numbers.split.map (&:to_i).minmax.reverse.join(" ")
 end
 
 def find_short(s)
