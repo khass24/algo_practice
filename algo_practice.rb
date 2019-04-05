ce56d766b6ba0074b42e36c47722682cdce3bf80
kyle-hassett <kylehassett72@gmail.com>
2019-04-05 14:41:22 -0500

    algo practice

--- algo_practice.rb
+++ algo_practice.rb
@@ -45,10 +45,10 @@ We can calculate John's average hourly speed on every section and we get:
 [45.6, 74.4, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0]
 Given s and x the task is to return as an integer the *floor* of the maximum average speed per hour obtained on the sections of x. If x length is less than or equal to 1 return 0 since the car didn't move.
 
-Example:
-with the above data your function gps(s, x)should return 74
+# Example:
+# with the above data your function gps(s, x)should return 74
 
-Note
-With floats it can happen that results depends on the operations order. To calculate hourly speed you can use:
+# Note
+# With floats it can happen that results depends on the operations order. To calculate hourly speed you can use:
 
-(3600 * delta_distance) / s
+# (3600 * delta_distance) / s
